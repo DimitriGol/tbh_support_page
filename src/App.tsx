8ec63f7e0d9aceb048aa70953884f5ea1d197fdb
Golyshev <dgolyshev@tbh.org>
2024-06-07 18:59:04 -0400

    Try to add tailwind. Does not work

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './stylesheet/App.css'
+import tbhLogo from './assets/logo_tag_desk.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={tbhLogo} alt="TBH Logo"/>
+        <h1 className="text-3xl">
+          Support 
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Start a remote session
       </p>
     </>
   )
